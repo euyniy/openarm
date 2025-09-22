9b10f6f14babc02f2295ae277d7b1835f57bd6fd
Sutou Kouhei <kou@clear-code.com>
2025-09-22 14:57:28 +0900

    website: Add meta tag to verify domain on Algoria (#261)

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -86,6 +86,9 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/social-card.svg',
+    metadata: [
+      {name: 'algolia-site-verification', content: 'F06502A35B61FF7C'},
+    ],
     navbar: {
       title: 'OpenArm',
       logo: {
